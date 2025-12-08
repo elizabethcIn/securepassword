27fe35bafb24a27b63ccd5c73198245f38e4c459
elizabethcIn <elizabethtousens@gmail.com>
2025-12-07 21:45:08 -0500

    password v 0.03

--- improve.py
+++ improve.py
@@ -13,3 +13,21 @@ for x in range(16):
     password += random.choice(chars)
 print(password)
 
+if len(password) >= 15:
+    print ("longitud correcta")
+else:
+    print ("longitud incorrecta")
+for letra in password:
+    if  letra in "capitalletters" :
+     capitalletters = True
+    if  letra in "lowercase" :
+     lowercase = True
+    if letra in "number" :
+     number = True
+    if letra in "special" :
+     special = True
+    
+
+
+
+
